82fe3ba41273b3c2a29b3c72f1c8668190ebea8e
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-03-03 15:21:16 +0530

    feat: 🎸 add  Introduction to State

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,20 @@
+import propTypes from 'prop-types';
+import React from 'react';
+
+class Slider extends React-Component{
+  constructor(props){
+    super(props);
+  }
+  Slider.propTypes = {
+    altText: propTypes.string,
+    banners: propTypes.array,
+    defaultBanner: propTypes.string,
+    duration: propTypes.number,
+    height: propTypes.number,
+    random: propTypes.number,
+  };
+  Slider.de
+  render(){
+    return();
+  }
+}
---
+++ src/lib/utils/math.js
@@ -0,0 +1,3 @@
+export function getRandomNumber(max) {
+  return (Math.random() * (max - 1));
+}
