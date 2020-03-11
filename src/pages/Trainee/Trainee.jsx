e7f0f616d933bdad52e57f5e9e148d4dc71a3b67
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-03-11 18:01:43 +0530

    feat: 🎸 add Material UI - Styling, Dialog, and Form Validation

--- src/pages/Trainee/Trainee.jsx
+++ src/pages/Trainee/Trainee.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import * as yup from 'yup';
 import { AddDialog } from './components';
 
-export class Trainee extends React.Component{
-  render(){
+export class Trainee extends React.Component {
+  render() {
     return (
       <AddDialog />
     );
--- src/pages/Trainee/components/AddDialog/AddDialog.jsx
+++ src/pages/Trainee/components/AddDialog/AddDialog.jsx
@@ -12,13 +12,81 @@ import {
 import EmailIcon from '@material-ui/icons/Email';
 import PersonIcon from '@material-ui/icons/Person';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import Grid from '@material-ui/core/Grid';
+import * as yup from 'yup';
 
 export class AddDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
+      name: '',
+      email: '',
+      password: '',
+      nameError: '',
+      emailError: '',
+      passwordError: '',
+      confirmPasswordError: '',
+      isTouch: false,
     };
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ name: event.target.value }, () => { this.getError('name'); });
+    this.isTouched();
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value }, () => { this.getError('email'); });
+    this.isTouched();
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value }, () => {
+      this.getError('password');
+      this.getError('confirmPassword');
+    });
+    this.isTouched();
+  }
+
+  hasErrors = () => {
+    const {
+      nameError, emailError, passwordError,
+    } = this.state;
+    return !!((nameError || emailError || passwordError));
+  }
+
+  isTouched = () => {
+    const {
+      name, email, password,
+    } = this.state;
+    this.setState({ isTouch: true });
+    return !!(name || email || password);
+  }
+
+  getError = (label) => {
+    const schema = yup.object().shape({
+      name: yup.string().required('Name is required field'),
+      email: yup.string().email().required('Email is required field'),
+      password: yup.string().required('Password is required field'),
+      // .matches('^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$',
+      //   'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character'),
+      confirmPassword: yup.string().required('Confirm Password is required field').oneOf([yup.ref('password'), null], 'Passwords must match'),
+    });
+    const key = `${[label]}Error`;
+    schema.validateAt(label, { [label]: this.state[label] })
+      .then(() => {
+        this.setState({ [key]: '' });
+      })
+      .catch((error) => {
+        this.setState({ [key]: error.errors });
+        return key;
+      });
+  }
+
+  isDisabled = () => {
+    const { isTouch } = this.state;
+    return (this.hasErrors() && isTouch) ? 'disabled' : '';
   }
 
   handleClickOpen = () => {
@@ -30,7 +98,10 @@ export class AddDialog extends React.Component {
   };
 
   render() {
-    const { open } = this.state;
+    console.log(this.state);
+    const {
+      open, nameError, emailError, passwordError, confirmPasswordError,
+    } = this.state;
     return (
       <div>
         <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
@@ -42,77 +113,96 @@ export class AddDialog extends React.Component {
             <DialogContentText>
               Enter Your Trainee Details
             </DialogContentText>
-            <div>
-              <TextField
-                required
-                id="name"
-                label="Name"
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <PersonIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                variant="outlined"
-              />
-            </div>
-            <br />
-            <div>
-              <TextField
-                required
-                id="email"
-                label="Email Address"
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                variant="outlined"
-              />
-            </div>
-            <br />
-            <div>
-              <TextField
-                id="password"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <VisibilityOffIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                variant="outlined"
-              />
-              &nbsp;&nbsp;
-              <TextField
-                id="confirm-password"
-                label="Confirm Password"
-                type="password"
-                autoComplete="current-password"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <VisibilityOffIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                variant="outlined"
-              />
-            </div>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  required
+                  id="name"
+                  label="Name"
+                  helperText={nameError}
+                  error={!!(nameError)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <PersonIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  variant="outlined"
+                  onChange={this.handleNameChange}
+                  onBlur={this.handleNameChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  required
+                  helperText={emailError}
+                  error={!!(emailError)}
+                  id="email"
+                  label="Email Address"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  variant="outlined"
+                  onChange={this.handleEmailChange}
+                  onBlur={this.handleEmailChange}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  helperText={passwordError}
+                  error={!!(passwordError)}
+                  id="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <VisibilityOffIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  variant="outlined"
+                  onChange={this.handlePasswordChange}
+                  onBlur={this.handlePasswordChange}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  helperText={confirmPasswordError}
+                  error={!!(confirmPasswordError)}
+                  id="confirm-password"
+                  label="Confirm Password"
+                  type="password"
+                  autoComplete="current-password"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <VisibilityOffIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  variant="outlined"
+                  onChange={this.handlePasswordChange}
+                  onBlur={this.handlePasswordChange}
+                />
+              </Grid>
+            </Grid>
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
               CANCEL
             </Button>
-            <Button onClick={this.handleClose} color="primary">
+            <Button onClick={this.handleClose} color="primary" disabled={this.isDisabled()}>
               SUBMIT
             </Button>
           </DialogActions>
