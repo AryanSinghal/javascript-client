a2a587c2c10c07cce1a36517874e27a794b4b441
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-03-13 19:15:29 +0530

    feat: 🎸 add Material UI - Styling, Dialog, and Form Validation

--- src/pages/Trainee/Trainee.jsx
+++ src/pages/Trainee/Trainee.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import * as yup from 'yup';
 import { AddDialog } from './components';
 
-export class Trainee extends React.Component {
-  render() {
-    return (
-      <AddDialog />
-    );
-  }
-}
+export const Trainee = () => {
+  return (
+    <AddDialog />
+  );
+};
