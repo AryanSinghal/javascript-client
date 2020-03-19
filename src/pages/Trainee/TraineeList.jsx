bc2a426182808d4cb678542fdba9ed76fd266e95
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-03-19 13:26:17 +0530

    feat: 🎸 add  Dynamic Routing

---
+++ src/pages/Trainee/TraineeList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TraineeList = () => {
+  return (
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  );
+};
+
+export default TraineeList;
