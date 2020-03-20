1a7be90df8d78b9966926d8f7df5412254669d2b
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-03-20 09:23:03 +0530

    feat: 🎸 add React - NavBar and Login UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { Trainee, Navbar } from './pages';
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <Navbar />
       <br />
       <Trainee />
