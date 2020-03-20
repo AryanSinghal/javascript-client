14916fc677c2c18f197ba1a609e476132d0b2aca
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-03-20 09:16:06 +0530

    feat: 🎸 add Dynamic Routing

--- src/pages/Trainee/TraineeDetails.jsx
+++ src/pages/Trainee/TraineeDetails.jsx
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import {
-  Typography, Card, CardContent, CardMedia,
+  Typography, Card, CardContent, CardMedia, Grid,
 } from '@material-ui/core';
 import moment from 'moment';
 import trainee from './data/trainee';
@@ -17,24 +17,33 @@ const TraineeDetails = () => {
   const { name, email, createdAt } = data[0];
   return (
     <Card>
-      <CardMedia
-        image="images/banners/cloud.jpg"
-        title="Live from space album cover"
-      />
-      <CardContent>
-        <Typography variant="h6">
-          {name}
-        </Typography>
-        <br />
-        <Typography variant="h8">
-          {moment(createdAt).format('dddd MMMM Do YY h:mm:ss a')}
-        </Typography>
-        <br />
-        <br />
-        <Typography gutterBottom variant="h7">
-          {email}
-        </Typography>
-      </CardContent>
+      <Grid container spacing={2}>
+        <Grid xs={2}>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image="/images/banners/cloud.jpg"
+            title="Contemplative Reptile"
+          />
+        </Grid>
+        <Grid xs={10}>
+          <CardContent>
+            <Typography variant="h6">
+              {name}
+            </Typography>
+            <br />
+            <Typography variant="h8">
+              {moment(createdAt).format('dddd MMMM Do YY h:mm:ss a')}
+            </Typography>
+            <br />
+            <br />
+            <Typography gutterBottom variant="h7">
+              {email}
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </Card>
   );
 };
