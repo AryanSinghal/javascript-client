6510677f2950c99797809185c8b4eb0a19cb61f3
AryanSinghalGit <aryan.singhal@successive.tech>
2020-05-31 17:15:00 +0530

    feat: 🎸 add changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,34 @@ import { CssBaseline } from '@material-ui/core';
 import {
   BrowserRouter as Router, Route, Redirect, Switch,
 } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
 import {
   Trainee, Login, NoMatch, TextFieldDemo, InputDemo, ChildrenDemo,
 } from './pages';
 import { SnackbarProvider } from './contexts';
 import { AuthRoute, PrivateRoute } from './routes';
+import { theme } from './theme';
 
 function App() {
   return (
     <SnackbarProvider>
-      <Router>
-        <CssBaseline />
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-          <AuthRoute exact path="/login" component={Login} />
-          <PrivateRoute path="/trainee" component={Trainee} />
-          <PrivateRoute exact path="/textfield-demo" component={TextFieldDemo} />
-          <PrivateRoute exact path="/input-demo" component={InputDemo} />
-          <PrivateRoute exact path="/children-demo" component={ChildrenDemo} />
-          <PrivateRoute component={NoMatch} />
-        </Switch>
-      </Router>
-    </SnackbarProvider>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CssBaseline />
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/login" />
+            </Route>
+            <AuthRoute exact path="/login" component={Login} />
+            <PrivateRoute path="/trainee" component={Trainee} />
+            <PrivateRoute exact path="/textfield-demo" component={TextFieldDemo} />
+            <PrivateRoute exact path="/input-demo" component={InputDemo} />
+            <PrivateRoute exact path="/children-demo" component={ChildrenDemo} />
+            <PrivateRoute component={NoMatch} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </SnackbarProvider >
   );
 }
 
