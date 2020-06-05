dad726fe9b98bd5501a45ec357f99dc400500a04
AryanSinghalGit <aryan.singhal@successive.tech>
2020-06-05 19:17:33 +0530

    feat: 🎸 add changes

--- src/theme.js
+++ src/theme.js
@@ -3,8 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
   typography: {
     fontFamily: [
-      "cursive",
-      'Comic Sans MS',
+      'monospace',
+      'cursive',
+      '"Comic Sans MS"',
       'sans-serif',
     ].join(','),
   },
