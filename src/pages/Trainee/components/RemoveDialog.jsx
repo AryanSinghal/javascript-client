4a3b9fc19451cd60df5b158fe2bce41dd47ae6d6
AryanSinghalGit <aryan.singhal@successive.tech>
2020-06-09 22:01:41 +0530

    feat: 🎸 add changes

--- src/pages/Trainee/components/RemoveDialog.jsx
+++ src/pages/Trainee/components/RemoveDialog.jsx
@@ -12,13 +12,9 @@ import { SnackbarConsumer } from '../../../contexts';
 import { SUCCESS_MESSAGE, FAILURE_MESSAGE } from '../../../configs/constants';
 
 export const RemoveDialog = (props) => {
-<<<<<<< HEAD
   const {
     open, onSubmit, onClose, progressBar,
   } = props;
-=======
-  const { open, onSubmit, onClose, data } = props;
->>>>>>> e17e7dfe67902287e1933b25e0653953a2165429
   return (
     <>
       <Dialog
@@ -41,10 +37,12 @@ export const RemoveDialog = (props) => {
               <Button
                 onClick={() => {
                   onSubmit(data);
-                  if (new Date(data.createdAt) >= new Date('2019-02-14T00:00:00'))
+                  if (new Date(data.createdAt) >= new Date('2019-02-14T00:00:00')) {
                     openSnackbar('success', SUCCESS_MESSAGE);
-                  else
+                  }
+                  else {
                     openSnackbar('error', FAILURE_MESSAGE);
+                  }
                 }}
                 color="primary"
                 autoFocus
@@ -63,9 +61,5 @@ RemoveDialog.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
-<<<<<<< HEAD
   progressBar: PropTypes.bool.isRequired,
-=======
-  data: PropTypes.object.isRequired,
->>>>>>> e17e7dfe67902287e1933b25e0653953a2165429
 };
